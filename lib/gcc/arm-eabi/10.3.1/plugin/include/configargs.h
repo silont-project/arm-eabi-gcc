f84e21ef0ac7124c59a24b3da49578308c961e48
Reinazhard <reinazhard@gmail.com>
2021-05-28 07:44:01 +0000

    [DroneCI]: NGenToD GCC 280521
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- lib/gcc/arm-eabi/10.3.1/plugin/include/configargs.h
+++ lib/gcc/arm-eabi/10.3.1/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --prefix=/drone/src/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Ngentod GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --prefix=/drone/src/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Ngentod GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot --with-zstd";
 static const char thread_model[] = "single";
 
 static const struct {
