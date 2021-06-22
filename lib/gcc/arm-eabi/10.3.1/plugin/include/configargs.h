3c658f460eeaaafca03c5fd62044b3bc3da0a856
Reinazhard <reinazhard@gmail.com>
2021-06-23 00:49:59 +0700

    Import ARM GCC 20210622
    Build completed on: Wed Jun 23 00:49:59 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm/bin/../libexec/gcc/arm-eabi/11.1.1/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Ngentod GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.1.1 20210622 (Ngentod GCC)
    
    LLD: LLD 13.0.0 (https://github.com/llvm/llvm-project b2f48cc9142012aeac16b8a28070f630e6711d5d) (compatible with GNU linkers)
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- lib/gcc/arm-eabi/10.3.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --prefix=/drone/src/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Ngentod GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot --with-zstd";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "arm7tdmi" } };
